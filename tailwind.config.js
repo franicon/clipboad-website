7da974e3e8c05c0b48be7b238296de094f100c5d
Abayomi Aluko <franicons@gmail.com>
2023-01-04 09:21:05 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm:'480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+      extends: {
+        // colors: {},
+        fontFamily: {
+          sans: ['Bai Jamjuree', 'sans-serif']
+        }
+      }
+    },
+  },
+  plugins: [],
+}
